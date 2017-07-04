c7ba08ee122a2ca33601c8058bf889360b459834
huangminglei <937138688@qq.com>
2017-07-04 23:59:07 +0800

    add poll 机制

---
+++ buttons_poll_app.c
@@ -0,0 +1,30 @@
+#include <...>//自己去man手册查
+
+int main(void)
+{
+
+	int fd = open("/dev/buttons", O_RDWR);
+	if (fd < 0) {
+		perror("open");
+		exit(1);
+	}
+
+	struct pollfd fds[1];
+	fds[0].fd = fd;
+	fds[0].events = POLLIN;
+
+
+	int ret;
+	unsigned char *key_val;
+	while (1) {
+		ret = poll(fds, 1, 5000);
+		if (ret == 0) 
+			printf("time out\n");
+		else {
+			read(fd, &key_val, 1);
+			printf("key_val = 0x%x\n", key_val);
+		}
+	}
+	
+	return 0;
+}
