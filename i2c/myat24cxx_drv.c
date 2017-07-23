07e5794f25726e65ca8e466112f22cf1b7bb0641
huangminglei <937138688@qq.com>
2017-07-24 00:02:46 +0800

    add file operations

--- i2c/myat24cxx_drv.c
+++ i2c/myat24cxx_drv.c
@@ -1,29 +1,82 @@
 #include <...>
 
 
+static int major;
+static struct class *class;
+static struct i2c_client *at24cxx_client;
+
+
+/*传入：buf[0]: addr
+  输出的数据存于：buf[1]: data
+*/
+static ssize_t at24cxx_read(struct file *file, char __user *buf, 
+								size_t count, loff_t *offset)
+{
+	unsigned char addr, data;
+	copy_from_user(&addr, buf, 1);
+	/*查看内核document下的smbus协议，对比硬件手册上的时序，找到对应的可用函数*/
+	data = i2c_smbus_read_byte_data(at24cxx_client, addr);
+	copy_to_user(buf[1], &data, 1);
+	return 1;
+}
+
+
+/*buf[0]: addr
+  buf[1]: data
+*/
+static ssize_t at24cxx_write(struct file *file, char __user *buf, 
+								size_t count, loff_t *offset)
+{
+	unsigned char ker_buf[2];
+	unsigned char addr, data;
+
+	copy_from_user(ker_buf, buf, 2);
+	addr = ker_buf[0];
+	data = ker_buf[1];
+	/*查看内核document下的smbus协议，对比硬件手册上的时序，找到对应的可用函数*/
+	if (!i2c_smbus_write_byte_data(at24cxx_client, addr, data))
+		return 2;
+	else
+		return -EIO;
+}
+
+
+static struct file_operations at24cxx_fops = {
+	.owner = THIS_MODULE,
+	.read = at24cxx_read,
+	.write = at24cxx_write,
+};
 
 
 static int __devinit at24cxx_probe(struct i2c_client *client,
 									const struct i2c_device_id *id)
 {
 	printk("%s %s %d\n", __FILE__, __func__, __line__);
+	at24cxx_client = client;
+	major = register_chrdev(0, "at24cxx", &at24cxx_fops);
+	class = class_create(THIS_MODULE, "at24cxx");
+	device_create(class, NULL， MKDEV(major, 0), NULL, "at24cxx");// dev/at24cxx
+
 	return 0;
 }
+
 
 static int __devinit at24cxx_remove(struct i2c_client *client,
 									const struct i2c_device_id *id)
 {
 	printk("%s %s %d\n", __FILE__, __func__, __line__);
+	device_destroy(clas, MKDEV(major, 0));
+	class_destroy(class);
+	unregister_chrdev(major, "at24cxx");
+
 	return 0;
 }
-
 
 
 static const struct i2c_device_id at24cxx_id_table[] = {
 		{"at24c08", 0},
 		{}
 };
-
 
 
 /*1.分配、设置i2c_driver*/
@@ -38,10 +91,6 @@ static struct i2c_driver at24cxx_driver = {
 };
 
 
-
-
-
-
 static int at24cxx_drv_init(void)
 {
 	/*2.注册 i2c_driver*/ 
@@ -49,7 +98,6 @@ static int at24cxx_drv_init(void)
 	return 0;
 }
 module_init(at24cxx_drv_init);
-
 
 
 static void at24cxx_drv_exit(void)
